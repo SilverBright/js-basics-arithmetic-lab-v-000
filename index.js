3990d03d7138743bcc84e3dd69451b71325469eb
Barbara Washington <barbara.m.washington@gmail.com>
2018-12-21 22:51:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = Math.random(0, 20);
 
-const randomInteger = Math.isInteger(randomNumber);
+const randomInteger = Number.isInteger(randomNumber);
